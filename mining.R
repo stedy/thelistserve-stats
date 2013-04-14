a982a9b36dba0e038ba488dac109342b2b3ccde1
Zach Stednick <zstednic@fhcrc.org>
2013-04-13 17:28:17 -0700

    cleaned up variable names

--- mining.R
+++ mining.R
@@ -1,9 +1,27 @@
-#text mining script for 
+#text mining script for The Listserve
 library(tm)
+library(chron)
+
+#determine names
+names <- data.frame(table(working$sender.name))
+names <- names[order(names[, 2], decreasing=TRUE), ]
+write.table(names, "names_table.txt", row.names = F, col.names=F, quote=F)
+
+working$first.names <- sapply(strsplit(as.character(working$sender.name), "\\ "), "[", 1)
+first.names <- data.frame(table(working$first.names))
+first.names <- first.names[order(first.names[, 2], decreasing=TRUE), ]
+
+#time sent
+
+arrival.times <- times(working$time)
+pdf("arrivaltimes.pdf", width=17, height=17)
+plot.ts(arrival.times, main="Arrival times based on UTC")
+abline(h=mean(arrival.times), col="red")
+dev.off()
 
 #first try cleaning subject line
 
-subject.corpus <- Corpus(VectorSource(test1))
+subject.corpus <- Corpus(VectorSource(raw.subjects))
 subject.corpus <- tm_map(subject.corpus, removePunctuation)
 subject.corpus <- tm_map(subject.corpus, removeWords, stopwords('english'))
 subject.ctdm <- TermDocumentMatrix(subject.corpus, control=list(removePunctuation=TRUE,
@@ -11,19 +29,21 @@ subject.ctdm <- TermDocumentMatrix(subject.corpus, control=list(removePunctuatio
                                                       stopwords = TRUE))
 subject.ctdm.m <- as.matrix(subject.ctdm)
 word.freqs.subject <- sort(rowSums(subject.ctdm.m), decreasing=TRUE) 
-# create a data frame with words and their frequencies
+
+# create a data frame with subject words and their frequencies
 subj.dm <- data.frame(word=names(word.freqs.subject), freq=word.freqs.subject)
 
-clean.corpus <- Corpus(VectorSource(test3$emailonly))
+#then repeat for the body of the email
+clean.corpus <- Corpus(VectorSource(working$emailonly))
 clean.corpus <- tm_map(clean.corpus, removePunctuation)
 clean.corpus <- tm_map(clean.corpus, removeWords, stopwords('english'))
 ctdm <- TermDocumentMatrix(clean.corpus, control=list(removePunctuation=TRUE,
                                                       removeNumbers = TRUE,
                                                       stopwords = TRUE))
-
-#get frequency of words
+#get frequency of words from body
 ctdm.m <- as.matrix(ctdm)
 word.freqs <- sort(rowSums(ctdm.m), decreasing=TRUE) 
+
 # create a data frame with words and their frequencies
 dm <- data.frame(word=names(word.freqs), freq=word.freqs)
 
@@ -34,7 +54,6 @@ dev.off()
 
 
 mydata.df <- as.data.frame(inspect(ctdm2))
-
 mydata.df.scale <- scale(mydata.df)
 d <- dist(mydata.df.scale, method = "euclidean") # distance matrix
 pdf("dendrogram.pdf", width=17, height=17)
@@ -42,5 +61,5 @@ fit <- hclust(d, method="ward")
 plot(fit)
 dev.off()  
 
-days <- seq(as.Date("2012-04-26"), as.Date("2013-02-27"), by="1 day")
-j <- setdiff(as.character(days), as.character(test2$date))
+days <- seq(as.Date("2012-04-18"), as.Date("2013-04-13"), by="1 day")
+j <- setdiff(as.character(days), as.character(raw.datecheck$date))
--- process.R
+++ process.R
@@ -10,27 +10,30 @@ raw$whom <- sapply(strsplit(raw$emailfull, "@"), "[", 1)
 raw$date <- sapply(strsplit(raw$dt, " "), "[", 1)
 raw$sender.name <- sapply(strsplit(raw$sender, '\\"'), "[", 2)
 raw$date <- as.Date(raw$date)
+raw$time <- sapply(strsplit(raw$dt, " "), "[", 2)
+
 raw$subjclean <- sapply(strsplit(raw$subject, "Listserve=5D"), "[", 2)
 
-test1 <- gsub("=20", ' ', raw$subjclean)
-test1 <- gsub("?=", '', test1, fixed=TRUE)
-test1 <- gsub("=5B", '[', test1)
-test1 <- gsub("=2D", '-', test1)
-test1 <- gsub("=5D", ']', test1)
-test1 <- gsub("=27", "'", test1)
-test1 <- gsub("=28", "(", test1)
-test1 <- gsub("=29", ")", test1)
-test1 <- gsub("=21", '!', test1)
-test1 <- gsub("=3F", '?', test1)
-test1 <- gsub("=3A", ':', test1)
-test1 <- gsub("=2C", ',', test1)
-test1 <- gsub("=3B", ";", test1)
-test1 <- gsub("=E2=80=99", "'", test1)
-test1 <- gsub("=E2=80=9C", '"', test1)
-test1 <- gsub("=E2=80=9D", '"', test1)
+raw.subjects <- gsub("=20", ' ', raw$subjclean)
+raw.subjects <- gsub("?=", '', raw.subjects, fixed=TRUE)
+raw.subjects <- gsub("=5B", '[', raw.subjects)
+raw.subjects <- gsub("=2D", '-', raw.subjects)
+raw.subjects <- gsub("=5D", ']', raw.subjects)
+raw.subjects <- gsub("=27", "'", raw.subjects)
+raw.subjects <- gsub("=28", "(", raw.subjects)
+raw.subjects <- gsub("=29", ")", raw.subjects)
+raw.subjects <- gsub("=21", '!', raw.subjects)
+raw.subjects <- gsub("=3F", '?', raw.subjects)
+raw.subjects <- gsub("=3A", ':', raw.subjects)
+raw.subjects <- gsub("=2C", ',', raw.subjects)
+raw.subjects <- gsub("=3B", ";", raw.subjects)
+raw.subjects <- gsub("=E2=80=99", "'", raw.subjects)
+raw.subjects <- gsub("=E2=80=9C", '"', raw.subjects)
+raw.subjects <- gsub("=E2=80=9D", '"', raw.subjects)
 
 #QC metrics
-test2 <- raw[raw$date >= as.Date("2012-04-25"), ]
-test3 <- test2[!duplicated(test2$sender), ]
+raw.datecheck <- raw[raw$date >= as.Date("2012-04-18"), ]
+working <- raw.datecheck[!duplicated(raw.datecheck$sender), ]
+working <- working[order(working$date), ]
 
 dbDisconnect(conn)
