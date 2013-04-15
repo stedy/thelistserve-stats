995b5e2eac8bb29feb1572db10c7a36abe18f73d
Zach Stednick <zstednic@fhcrc.org>
2013-04-14 22:30:14 -0700

    fixed bug causing multiple entries

--- process.R
+++ process.R
@@ -12,9 +12,14 @@ raw$sender.name <- sapply(strsplit(raw$sender, '\\"'), "[", 2)
 raw$date <- as.Date(raw$date)
 raw$time <- sapply(strsplit(raw$dt, " "), "[", 2)
 
-raw$subjclean <- sapply(strsplit(raw$subject, "Listserve=5D"), "[", 2)
+#remove duplicates and clean subject line
+raw.datecheck <- raw[raw$date >= as.Date("2012-04-18"), ]
+working <- raw.datecheck[!duplicated(raw.datecheck$sender), ]
+working <- working[order(working$date), ]
 
-raw.subjects <- gsub("=20", ' ', raw$subjclean)
+working$subjclean <- sapply(strsplit(working$subject, "Listserve=5D"), "[", 2)
+
+raw.subjects <- gsub("=20", ' ', working$subjclean)
 raw.subjects <- gsub("?=", '', raw.subjects, fixed=TRUE)
 raw.subjects <- gsub("=5B", '[', raw.subjects)
 raw.subjects <- gsub("=2D", '-', raw.subjects)
@@ -30,10 +35,5 @@ raw.subjects <- gsub("=3B", ";", raw.subjects)
 raw.subjects <- gsub("=E2=80=99", "'", raw.subjects)
 raw.subjects <- gsub("=E2=80=9C", '"', raw.subjects)
 raw.subjects <- gsub("=E2=80=9D", '"', raw.subjects)
-
-#QC metrics
-raw.datecheck <- raw[raw$date >= as.Date("2012-04-18"), ]
-working <- raw.datecheck[!duplicated(raw.datecheck$sender), ]
-working <- working[order(working$date), ]
 
 dbDisconnect(conn)
